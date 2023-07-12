410bd8d128ad083ffaba6dac531cb81430c5922b
Thalles Rocha <thalles_leopoldino@outlook.com>
2023-07-12 11:13:56 -0300

    Atualização de usuário

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value="/users")
 public class UserResource {
 	
 	@Autowired
@@ -37,7 +38,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDTO);
 	}
 
-	@GetMapping(value = "/{id}")
+	@GetMapping(value="/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User user = service.findById(id);
 		UserDTO userDTO = new UserDTO(user);
@@ -53,9 +54,17 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value="/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO userDto, @PathVariable String id) {
+		User obj = service.fromDTO(userDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 	
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -26,20 +26,33 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id); 
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
 	}
 	
 	public User insert(User obj) {
 		return repository.insert(obj);
-	}
-	
-	public User fromDTO(UserDTO userDTO) {
-		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+		
 	}
 	
 	public void delete(String id) {
 		User user = findById(id);
 		repository.deleteById(user.getId());
 		
+	}
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+	
+	public void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 	
 	
