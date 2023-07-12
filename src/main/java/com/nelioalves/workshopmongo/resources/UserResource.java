a1c3a2b601d88e17bb639df51e6917881c876652
Thalles Rocha <thalles_leopoldino@outlook.com>
2023-07-12 10:20:09 -0300

    Implementando FindById no usuário

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResource.java
@@ -1,13 +1,16 @@
 package com.nelioalves.workshopmongo.resources;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 
@@ -20,8 +23,21 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
-		service.findAll();
-		return ResponseEntity.ok().body(service.findAll());
+		List<User> list = service.findAll();
+		List<UserDTO> listDTO = new ArrayList<>();
+		for (User x : list) {
+			listDTO.add(new UserDTO(x));
+		}
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		
+		return ResponseEntity.ok().body(userDTO);
 		
 	}
 
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,14 +1,14 @@
 package com.nelioalves.workshopmongo.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
-import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.repositories.UserRepository;
+import com.nelioalves.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,15 +16,15 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
-	public List<UserDTO> findAll() {
-		
+	public List<User> findAll() {
 		List<User> listUser = repository.findAll();
-		List<UserDTO> listDTO = new ArrayList<>();
-		for (User x : listUser) {
-			listDTO.add(new UserDTO(x));
-		}
-		return listDTO;
 		
+		return listUser;
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id); 
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
