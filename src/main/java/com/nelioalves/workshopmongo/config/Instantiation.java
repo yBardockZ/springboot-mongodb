cff8729470feafbb68d140376c729f4be21e43f9
Thalles Rocha <thalles_leopoldino@outlook.com>
2023-07-18 14:55:14 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.nelioalves.workshopmongo.domain.Post;
 import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 import com.nelioalves.workshopmongo.repositories.PostRepository;
 import com.nelioalves.workshopmongo.repositories.UserRepository;
 
@@ -45,6 +46,16 @@ public class Instantiation implements CommandLineRunner {
 		
 		maria.getPosts().addAll(Arrays.asList(post, post2));
 		userRepository.save(maria);
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));		
+		postRepository.saveAll(Arrays.asList(post, post2));
+		
+		
 	}
 
 }
