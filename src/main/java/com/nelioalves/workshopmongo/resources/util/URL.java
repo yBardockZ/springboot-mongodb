03095f33dae473557e148858e91c24e67e5ae57c
Thalles Rocha <thalles_leopoldino@outlook.com>
2023-07-20 03:46:09 -0300

    Consulta com varios critérios

--- src/main/java/com/nelioalves/workshopmongo/resources/util/URL.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/util/URL.java
@@ -1,6 +1,10 @@
 package com.nelioalves.workshopmongo.resources.util;
 
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	
@@ -12,6 +16,16 @@ public class URL {
 			return null;
 		}
 		
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}		
 	}
 	
 }
